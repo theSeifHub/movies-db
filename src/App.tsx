9a261b09cf543ebd44fb77a1c2d2e77b89925d9c
Seif Eldeen Ahmed <seifahmed870@yahoo.com>
2023-09-07 02:14:55 +0300

    init project structure

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Movies DB</h1>
+      <main className='board' />
+    </div>
+  );
+}
+
+export default App;
